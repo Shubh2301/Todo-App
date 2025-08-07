5498016fa776ccfdd327626aa7c58b6b1e1ebbff
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-08-07 20:53:16 +0530

    first commit

---
+++ src/projects/Todo/Todo.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import { MdCheck,MdDeleteOutline} from "react-icons/md";
+import "./Todo.css"
+
+
+export const Todo = () => {
+    const[inputValue,setInputValue]=useState("");
+    const[task,setTask]=useState([]);
+
+    const handleInputChange=(value)=>{
+        setInputValue(value);
+    }
+    const handleFormSubmit=(event)=>{
+        event.preventDefault();
+
+        if(!inputValue)return;
+
+        if(task.includes(inputValue)){
+            setInputValue("");
+            return;
+        }
+
+        setTask((prevTask)=>[...prevTask,inputValue]);
+        
+        setInputValue("");
+    }
+
+
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <section className="form">
+                <form onSubmit={handleFormSubmit}>
+                   <input type="text" autoComplete="off" className="todo-input" value={inputValue} onChange={(event)=>handleInputChange(event.target.value)} />
+                    <button type="submit" className="todo-btn">Add Task</button>
+                </form>
+            </section>
+            <section className="myUnOrdList">
+                <ul>
+                    {
+                        task.map((currTask,idx)=>{
+                            return<li key={idx} className="todo-item">
+                                <span>{currTask}</span>
+                                <button className="check-btn">
+                                    <MdCheck/>
+                                </button>
+                                <button className="delete-btn">
+                                    <MdDeleteOutline/>
+                                </button>
+                            </li>
+                        })
+                    }
+                </ul>
+
+            </section>
+        </section>
+    )
+}
