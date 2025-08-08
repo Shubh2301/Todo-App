59716b1ddd285d537266fa81fe15228051c12b26
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-08-08 14:55:21 +0530

    Date & Time Functionality

--- src/projects/Todo/Todo.jsx
+++ src/projects/Todo/Todo.jsx
@@ -1,53 +1,75 @@
-import { useState } from "react"
-import { MdCheck,MdDeleteOutline} from "react-icons/md";
+import { useEffect, useState } from "react"
+import { MdCheck, MdDeleteOutline } from "react-icons/md";
 import "./Todo.css"
 
 
 export const Todo = () => {
-    const[inputValue,setInputValue]=useState("");
-    const[task,setTask]=useState([]);
+    const [inputValue, setInputValue] = useState("");
 
-    const handleInputChange=(value)=>{
+    const [task, setTask] = useState([]);
+
+    const[dateTime,setDateTime]=useState("")
+
+
+    const handleInputChange = (value) => {
         setInputValue(value);
     }
-    const handleFormSubmit=(event)=>{
+    const handleFormSubmit = (event) => {
         event.preventDefault();
 
-        if(!inputValue)return;
+        if (!inputValue) return;
 
-        if(task.includes(inputValue)){
+        if (task.includes(inputValue)) {
             setInputValue("");
             return;
         }
 
-        setTask((prevTask)=>[...prevTask,inputValue]);
-        
+        setTask((prevTask) => [...prevTask, inputValue]);
+
         setInputValue("");
     }
 
+    //todo date and time
+
+    useEffect(()=>{
+        const interval=setInterval(() => {
+        const now = new Date();
+        const updatedDate = now.toLocaleDateString();
+        const updatedTime = now.toLocaleTimeString();
+        setDateTime(`${updatedDate} - ${updatedTime}`)
+    }, 1000)
+    return()=>clearInterval(interval);
+
+
+    },[]);
+
+
+
+    
 
     return (
         <section className="todo-container">
             <header>
                 <h1>Todo List</h1>
+                <h2 className="date-time">{dateTime}</h2>
             </header>
             <section className="form">
                 <form onSubmit={handleFormSubmit}>
-                   <input type="text" autoComplete="off" className="todo-input" value={inputValue} onChange={(event)=>handleInputChange(event.target.value)} />
+                    <input type="text" autoComplete="off" className="todo-input" value={inputValue} onChange={(event) => handleInputChange(event.target.value)} />
                     <button type="submit" className="todo-btn">Add Task</button>
                 </form>
             </section>
             <section className="myUnOrdList">
                 <ul>
                     {
-                        task.map((currTask,idx)=>{
-                            return<li key={idx} className="todo-item">
+                        task.map((currTask, idx) => {
+                            return <li key={idx} className="todo-item">
                                 <span>{currTask}</span>
                                 <button className="check-btn">
-                                    <MdCheck/>
+                                    <MdCheck />
                                 </button>
                                 <button className="delete-btn">
-                                    <MdDeleteOutline/>
+                                    <MdDeleteOutline />
                                 </button>
                             </li>
                         })
